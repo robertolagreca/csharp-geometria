69929868cd514273cd62f8b29951da66cc644ec0
robertolagreca <robertolagreca92@gmail.com>
2022-11-25 15:20:11 +0100

    Metodo calcolaArea

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo.
+//Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono,
+//rispettivamente, l’area e il perimetro del rettangolo.
+//Poi, nel vostro programma principale (Program.cs) chiedete all’utente di inserire,
+//da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo.
+//Dopo averlo istanziato, occupatevi di configurare le sue proprietà di base e altezza
+//(come abbiamo fatto con le nostre auto questa mattina nel programma principale) e
+//stampate a video il perimetro e l’area.
+//Provate ad istanziare tanti rettangoli con nome diverso e attributi diversi
+//(baseRettangolo e altezzaRettangolo) e provate a stampare le loro proprietà
+//ad esempio con un metodo StampaRettangolo che vi stampi il vostro rettangolo ad esempio cosi:
+//—— Rettangolo1 ——
+//base: 20 cm
+//altezza: 10 cm
+//Perimetro: 60 cm
+//Area: 200 cm2
+
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+        
+            int baseRettangolo;
+            int altezzaRettangolo;
+
+            public int calcolaArea(int b, int h)
+            {
+            int area;
+            area = (b * h) / 2;
+
+            return area;
+            }
+        
+    }
+}
